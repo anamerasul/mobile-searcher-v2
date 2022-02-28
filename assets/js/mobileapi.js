d64e43bd959a42022f69c970653c4c6d0979e882
anamerasul <anamerasul@gmail.com>
2022-03-01 01:10:58 +0600

    display twenty phone from  all phone section html section add

--- assets/js/mobileapi.js
+++ assets/js/mobileapi.js
@@ -9,7 +9,7 @@ const searchMobileByName=(searchInputText)=>{
 
         fetch(url)
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>DisplayAllPhone(data))
 }
 
 // searchButtonHander add
@@ -28,8 +28,53 @@ document.getElementById(searchbtnid).addEventListener('click', function(e){
         console.log(searchInputvalue)
 
         searchMobileByName(searchInputvalue)
+
+        searchInput.value='';
 })
 
+}
+
+
+// display all mobile by searchName function add
+const DisplayAllPhone=(phone)=>{
+        console.log(phone)
+
+        const allPhones=phone.data
+        const notFound=document.getElementById('not-found')
+
+        const displaytwentyPhoneRowDiv=document.getElementById('display-twenty-phone-row')
+        if(phone.status===false){
+                notFound.innerHTML=`<p class="not-found-color">your mobile is not found please search right name</p>`
+        }
+        else{
+                // get only 20 phone
+                const phoneslice=allPhones.slice(0,20)
+                // const phones=allPhones.slice(0,19).forEach(phone=>{
+                //         console.log(phone)
+                // })
+                for(const phone of phoneslice){
+
+                        const colDiv=document.createElement('div');
+                        colDiv.classList.add('col')
+
+                        console.log(phone)
+
+                        colDiv.innerHTML=`<div class="card">
+                          <img src="..." class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          </div>
+                        </div>`
+
+                        displaytwentyPhoneRowDiv.appendChild(colDiv)
+                }
+
+        console.log(phone.data)
+
+        notFound.textContent='';
+
+}
 }
 
 searchButtonHander('search-btn','search-input')
